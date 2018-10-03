bf6df2687fccedf8d4467a20cac02d1f31901588
Monica Tamaru Jones <mntamaru@gmail.com>
2018-10-03 21:09:15 +0000

    automatically backed up by learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -5,7 +5,6 @@ import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js
 export default class ChromeBoisDomain extends Component {
 
   handleMouseMove = (event) => {
-
      drawChromeBoiAtCoords(event.clientX, event.clientY);
   }
 
